cce255c2a695c20f9748191da216d591290fe726
Varun Pereira <68728165+varunpereira@users.noreply.github.com>
2023-04-12 10:03:35 +1000

    put remaining pages

---
+++ src/feat/$/generic/home.jsx
@@ -0,0 +1,42 @@
+import {
+	title,
+	state,
+	react,
+	mount,
+	clean,
+	write,
+	d,
+	t,
+	b,
+	route,
+	db,
+	v,
+	p,
+	timer,
+	globe,
+	auth,
+} from "~/config/store"
+import batman_pic from '~/config/asset/batman.jpg'
+
+export default () => {
+	var car = state([batman_pic, "/home/2.jpg",])
+	var car_index = state(0)
+	var car_interv = timer().put(() => {
+		car_index((i) => (i + 1) % car().length)
+	}, 3000)
+	var nav = route()
+	var mute = state(true)
+
+	mount(async () => {
+		await auth("pub")
+	})
+
+	clean(() => {
+		timer().cut(car_interv)
+	})
+
+	return d(
+		{style: () => "fit_2 c_white"},
+		title({}, () => "Home - iStuff"),
+	)
+}
---
+++ src/feat/$/search/api/result.jsx
@@ -0,0 +1,32 @@
+import {write, db, env, res, num} from "~/config/store"
+import product_model from "~/config/db/model/product"
+
+export var POST = async ({request}) => {
+	var {search, categ, page} = await request.json()
+	search = search.trim()
+	page = num(page)
+	var products = []
+	db()
+	if (categ === "All") {
+		products = await product_model.find({
+			title: {$regex: search, $options: "i"},
+			approved: "true",
+		})
+	} else if (categ === "Tech") {
+		products = await product_model.find({
+			title: {$regex: search, $options: "i"},
+			category: "Tech",
+			approved: "true",
+		})
+	}
+	var products_per_page = 8
+	var pages = Math.ceil(products.length / products_per_page)
+	var lower = products_per_page * (page - 1)
+	var upper = products_per_page * page
+	products = products.slice(lower, upper)
+
+	return res({
+		pages,
+		products,
+	})
+}
