b340fe1c600dd058e2595cd82583f0491b140cac
Varun Pereira <68728165+varunpereira@users.noreply.github.com>
2023-04-18 15:21:40 +1000

    d

--- src/feat/$/cart/api/stripe.jsx
+++ src/feat/$/cart/api/stripe.jsx
@@ -20,7 +20,16 @@ export var POST = async ({request}) => {
 		var sesh = await stripe.checkout.sessions.create({
 			mode: "payment",
 			payment_method_types: ["card"],
-			line_items: [],
+			line_items: [{
+				price_data: {
+					currency: 'usd',
+					product_data: {
+						name: 'T-shirt',
+					},
+					unit_amount: 2000,
+				},
+				quantity: 1,
+			}],
 			success_url:
 				"https://" +
 				env.VITE_domain +
