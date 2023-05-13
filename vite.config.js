7f02e66c00ec0e33705381e65c2959aba8db1f69
Varun Pereira <68728165+varunpereira@users.noreply.github.com>
2023-05-13 11:50:02 +1000

    d

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,8 @@ export default defineConfig({
 			// appRoot: "./app/",
 			// serverEntry: './src/config/server' + (process.env.NODE_ENV === 'production' ? '.jsx' : ''),
 			// clientEntry: './src/client' + (process.env.NODE_ENV === 'production' ? '.jsx' : ''),
-			rootEntry: "./src/root" + (process.env.NODE_ENV === "production" ? ".jsx" : ""),
-			routesDir: './be/api',
+			rootEntry: "./src/fe/config/root" + (process.env.NODE_ENV === "production" && ".jsx"),
+			routesDir: "./be/api",
 		}),
 	],
 })
