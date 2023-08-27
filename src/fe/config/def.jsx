ea837be22241ad2333488fa786d6f0a9c6f60fdc
Varun Pereira <68728165+varunpereira@users.noreply.github.com>
2023-08-27 11:56:41 +1000

    d

--- src/fe/config/def.jsx
+++ src/fe/config/def.jsx
@@ -14,12 +14,18 @@ import {
 	globe,
 	title,
 } from "~/fe/config/shop"
+import {auth} from "~/fe/config/auth"
 
 export default () => {
 	var nav = route()
+
+	mount(async () => {
+		await auth("pub")
+	})
+
 	return d(
 		{style: () => "fit_1"},
-	title({},()=>"404 Error"),
+		title({}, () => "404 Error"),
 		t({}, () => "404 Error - Page not found"),
 		b({click: () => nav("/"), style: () => "hover:tc_grey"}, () => "Go Home"),
 	)
