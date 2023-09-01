aa72b415733bf28472c9b48ee1769df41f1b1f68
Varun Pereira <68728165+varunpereira@users.noreply.github.com>
2023-09-01 20:51:00 +1000

    b

--- src/fe/generic/home.jsx
+++ src/fe/generic/home.jsx
@@ -19,8 +19,9 @@ import car1 from "~/fe/generic/asset/1.jpg"
 import car2 from "~/fe/generic/asset/2.jpg"
 import car3 from "~/fe/generic/asset/3.jpg"
 import car4 from "~/fe/generic/asset/4.jpg"
-import lotr_vid from "~/fe/generic/asset/lotr_2.mp4"
-import lotr from "~/fe/generic/asset/lotr_2.png"
+import lotr_vid from "~/fe/generic/asset/lotr_1.mp4"
+import lotr from "~/fe/generic/asset/lotr_1.png"
+import lotr_logo from "~/fe/generic/asset/lotr_1_logo.png"
 
 export default () => {
 	var car = state([car1, car2, car3, car4])
@@ -30,6 +31,7 @@ export default () => {
 	}, 3000)
 	var nav = route()
 	var mute = state(true)
+	var logo = state(false)
 
 	mount(async () => {
 		await auth("pub")
@@ -39,22 +41,38 @@ export default () => {
 		timer.cut(car_interv)
 	})
 
+	var hover_in = (e) => {
+		e.target.play()
+		logo(true)
+	}
+
+	var hover_out = (e) => {
+		e.target.pause()
+		logo(false)
+	}
+
 	return d(
 		{},
 		title({}, () => "Home"),
-		v({
-			def: () => lotr,
-			value: () => lotr_vid,
-			mute: () => mute(),
-			hover_in: (e) => e.target.play(),
-			hover_out: (e) => e.target.pause(),
-			click: () => mute(false),
-			style: () => "fit_3 w_full",
-		}),
+		d(
+			{style: () => "z_fit"},
+			v({
+				def: () => lotr,
+				value: () => lotr_vid,
+				mute: () => mute(),
+				hover_in,
+				hover_out,
+				click: () => mute(false),
+				style: () => "fit_3 w_full h-[40vw] e_full overflow-hidden",
+			}),
+			p({
+				value: () => lotr_logo,
+				style: () => "z_put bottom-[.25rem] w-[50%] " + (logo() === true ? "see" : "hide"),
+			}),
+		),
 		t(
 			{
-				style: () =>
-					"fit_1 px-[1rem] py-[1rem] a_row ay_mid my-[3rem] tc_aqua ts_3 tw_2",
+				style: () => "fit_1 px-[1rem] py-[1rem] a_row ay_mid my-[3rem] tc_aqua ts_3 tw_2",
 			},
 			() => "Trending",
 		),
