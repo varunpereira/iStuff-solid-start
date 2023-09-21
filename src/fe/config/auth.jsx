a17c303586cd7287c29e9c02f2d5e1f0964f58c2
Varun Pereira <68728165+varunpereira@users.noreply.github.com>
2023-09-21 21:36:22 +1000

    d

--- src/fe/config/auth.jsx
+++ src/fe/config/auth.jsx
@@ -1,11 +1,11 @@
 import {req, write, globe, nav_full, path} from "~/fe/config/shop"
 
 export var auth = async (link) => {
-	var path_get = path.get()
-	link !== "pub" && path_get !== "/signin" ? nav_full("/signin") : ""
 	try {
 		var res = await req("/login/auth_get")
 		// write(res?.user)
+		var path_get = path.get()
+		link !== "pub" && path_get !== "/signin" && res?.user?.email == null ? nav_full("/signin") : ""
 		return globe({
 			email: !res?.user?.email?.startsWith("@") ? res?.user?.email : null,
 			cart_size: res?.cart_size,
